c4ec6f4a27b771c14c07871392cb664acb5aba73
Moataz Mohammady <moataz.mohammady@gmail.com>
2025-01-22 09:11:42 +0200

    move Projects to top

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,8 +21,8 @@ const IndexPage = () => {
       >
         <Box bgGradient="linear(to-r, gray.100, gray.300)">
           <About />
-          <Experience />
           <Projects />
+          <Experience />
           <Awards />
         </Box>
         <Footer />
